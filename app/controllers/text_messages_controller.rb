d5376ce9477c895acfcb41c4844a82b57f34c23c
Drew Land <drewmland@gmail.com>
2015-03-08 19:32:23 -0400

    Add disable XSRF and add a create method to text_messages_controller

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -1,2 +1,16 @@
 class TextMessagesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    TextMessage.create(message_attributes)
+    head :ok
+  end
+
+  private
+
+  def message_attributes
+    { to: params[:To],
+    from: params[:From],
+    body: params[:Body].strip.downcase }
+  end
 end
