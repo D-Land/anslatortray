3103f38dd4780f6a08a7d0391ff43f93079f6c5b
Drew Land <drewmland@gmail.com>
2015-03-08 19:35:19 -0400

    Add route for /text to post to text_message_controller#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  post 'text', to: 'text_messages#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
